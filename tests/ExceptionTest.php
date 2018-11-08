e0857070457fe2db4a276c3ca12224eaaf2d07af
Timo Schinkel <timo.schinkel@coolblue.nl>
2018-11-08 15:58:35 +0100

    Update `ExceptionTest` for the switch to php 7 - Replace `PHPUnit_Framework_TestCase` with `TestCase` - Replace `setExpectedException()` with `expectException()` - Require phpunit ^6.0 || ^7.0 to be compatible with all supported php versions

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -9,11 +9,12 @@ use GuzzleHttp\Psr7\Request as Psr7Request;
 use GuzzleHttp\Stream\Stream;
 use Http\Adapter\Guzzle5\Client;
 use Http\Message\MessageFactory\GuzzleMessageFactory;
+use PHPUnit\Framework\TestCase;
 
 /**
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
-class ExceptionTest extends \PHPUnit_Framework_TestCase
+class ExceptionTest extends TestCase
 {
     private $guzzleRequest;
 
@@ -38,84 +39,84 @@ class ExceptionTest extends \PHPUnit_Framework_TestCase
     public function testConnectException()
     {
         // Guzzle's ConnectException should be converted to a NetworkException
-        $this->setExpectedException('Http\Client\Exception\NetworkException');
+        $this->expectException('Http\Client\Exception\NetworkException');
         $this->makeRequest(new GuzzleExceptions\ConnectException('foo', $this->guzzleRequest));
     }
 
     public function testTooManyRedirectsException()
     {
         // Guzzle's TooManyRedirectsException should be converted to a RequestException
-        $this->setExpectedException('Http\Client\Exception\RequestException');
+        $this->expectException('Http\Client\Exception\RequestException');
         $this->makeRequest(new GuzzleExceptions\TooManyRedirectsException('foo', $this->guzzleRequest));
     }
 
     public function testRequestException()
     {
         // Guzzle's RequestException should be converted to a HttpException
-        $this->setExpectedException('Http\Client\Exception\HttpException');
+        $this->expectException('Http\Client\Exception\HttpException');
         $this->makeRequest(new GuzzleExceptions\RequestException('foo', $this->guzzleRequest, $this->guzzleResponse));
     }
 
     public function testRequestExceptionWithoutResponse()
     {
         // Guzzle's RequestException with no response should be converted to a RequestException
-        $this->setExpectedException('Http\Client\Exception\RequestException');
+        $this->expectException('Http\Client\Exception\RequestException');
         $this->makeRequest(new GuzzleExceptions\RequestException('foo', $this->guzzleRequest));
     }
 
     public function testBadResponseException()
     {
         // Guzzle's BadResponseException should be converted to a HttpException
-        $this->setExpectedException('Http\Client\Exception\HttpException');
+        $this->expectException('Http\Client\Exception\HttpException');
         $this->makeRequest(new GuzzleExceptions\BadResponseException('foo', $this->guzzleRequest, $this->guzzleResponse));
     }
 
     public function testBadResponseExceptionWithoutResponse()
     {
         // Guzzle's BadResponseException with no response should be converted to a RequestException
-        $this->setExpectedException('Http\Client\Exception\RequestException');
+        $this->expectException('Http\Client\Exception\RequestException');
         $this->makeRequest(new GuzzleExceptions\BadResponseException('foo', $this->guzzleRequest));
     }
 
     public function testClientException()
     {
         // Guzzle's ClientException should be converted to a HttpException
-        $this->setExpectedException('Http\Client\Exception\HttpException');
+        $this->expectException('Http\Client\Exception\HttpException');
         $this->makeRequest(new GuzzleExceptions\ClientException('foo', $this->guzzleRequest, $this->guzzleResponse));
     }
 
     public function testClientExceptionWithoutResponse()
     {
         // Guzzle's ClientException with no response should be converted to a RequestException
-        $this->setExpectedException('Http\Client\Exception\RequestException');
+        $this->expectException('Http\Client\Exception\RequestException');
         $this->makeRequest(new GuzzleExceptions\ClientException('foo', $this->guzzleRequest));
     }
 
     public function testServerException()
     {
         // Guzzle's ServerException should be converted to a HttpException
-        $this->setExpectedException('Http\Client\Exception\HttpException');
+        $this->expectException('Http\Client\Exception\HttpException');
         $this->makeRequest(new GuzzleExceptions\ServerException('foo', $this->guzzleRequest, $this->guzzleResponse));
     }
 
     public function testServerExceptionWithoutResponse()
     {
         // Guzzle's ServerException with no response should be converted to a RequestException
-        $this->setExpectedException('Http\Client\Exception\RequestException');
+        $this->expectException('Http\Client\Exception\RequestException');
         $this->makeRequest(new GuzzleExceptions\BadResponseException('foo', $this->guzzleRequest));
     }
 
     public function testTransferException()
     {
         // Guzzle's TransferException should be converted to a TransferException
-        $this->setExpectedException('Http\Client\Exception\TransferException');
+        $this->expectException('Http\Client\Exception\TransferException');
         $this->makeRequest(new GuzzleExceptions\TransferException('foo'));
     }
 
     public function testParseException()
     {
         // Guzzle's ParseException should be converted to a TransferException
-        $this->setExpectedException('Http\Client\Exception\TransferException');
+        $this->expectException('Http\Client\Exception\TransferException');
         $this->makeRequest(new GuzzleExceptions\ParseException('foo', $this->guzzleResponse));
     }
 }
